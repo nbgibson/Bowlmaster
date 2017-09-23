4f63dc60712f612453dd3abb2b52eb68924551d6
WarriusBird <nbgibson@crimson.ua.edu>
2017-09-22 23:04:00 -0400

    Pins, Play space, ball, gravity

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public Vector3 launchSpeed;
+
+    private Rigidbody rigidbody;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        Launch();
+    }
+
+    public void Launch()
+    {
+        rigidbody.velocity = launchSpeed;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+    }
+}
