71529de634e7fb924c83e321b9e36b151f44a5c6
WarriusBird <nbgibson@crimson.ua.edu>
2017-10-02 21:43:04 -0400

    Added logic to track number of standing pins

--- Assets/Scripts/Pin.cs
+++ Assets/Scripts/Pin.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Pin : MonoBehaviour {
 
     public float standingThreshold;
-    public Rigidbody pin;
+    
 
 	// Use this for initialization
 	void Start () {
-        pin = GetComponent<Rigidbody>();
+        
 	}
 	
 	// Update is called once per frame
@@ -20,16 +20,17 @@ public class Pin : MonoBehaviour {
     public bool IsStanding()
     {
         Vector3 rotationInEuler = transform.rotation.eulerAngles;
+
         float tiltInX = Mathf.Abs(rotationInEuler.x);
         float tiltInZ = Mathf.Abs(rotationInEuler.z);
 
         if(tiltInX < standingThreshold && tiltInZ < standingThreshold)
         {
-            Debug.Log(name + " standing");
+            
             return true;
         }
 
-        Debug.Log(name + " fallen");
+        
         return false;
     }
 }
