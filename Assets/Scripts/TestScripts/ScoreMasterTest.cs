06d93cdc5f36cb0770455eca6df8c1683d2ed28f
WarriusBird <nbgibson@crimson.ua.edu>
2017-11-10 15:48:54 -0500

    First pass at unit testing

---
+++ Assets/Scripts/TestScripts/ScoreMasterTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+[TestFixture]
+public class ScoreTest
+{
+    [Test]
+    public void PassingTest()
+    {
+        Assert.AreEqual(1, 1);
+    }    
+}
