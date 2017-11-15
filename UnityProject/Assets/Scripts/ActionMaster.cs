b1fefe5f6f968a264ab4566d2a9fb5aedd450b53
WarriusBird <nbgibson@crimson.ua.edu>
2017-11-14 23:29:31 -0500

    Additional Tests written

--- UnityProject/Assets/Scripts/ActionMaster.cs
+++ UnityProject/Assets/Scripts/ActionMaster.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActionMaster : MonoBehaviour {
+public class ActionMaster {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public enum Action {Tidy, Reset, EndTurn, EndGame};
+
+    public Action Bowl (int pins)
+    {
+        if(pins < 0 || pins > 10)
+        {
+            throw new UnityException("Invalid pins");
+        }
+
+        if(pins == 10)
+        {
+            return Action.EndTurn;
+        }
+
+        throw new UnityException("Not sure what action to return!");
+    }
 }
--- UnityProject/Assets/Scripts/TestScripts/ActionMasterTest.cs
+++ UnityProject/Assets/Scripts/TestScripts/ActionMasterTest.cs
@@ -8,9 +8,18 @@ using System.Collections;
 [TestFixture]
 public class ActionMasterTest
 {
+    private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+
     [Test]
     public void PassingTest()
     {
         Assert.AreEqual(1, 1);
     }    
+
+    [Test]
+    public void T01OneStrikeReturnsEndTurn()
+    {
+        ActionMaster actionMaster = new ActionMaster();
+        Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+    }
 }
