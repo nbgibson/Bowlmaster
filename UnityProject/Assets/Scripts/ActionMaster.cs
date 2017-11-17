2f1dc361b653c50f21b2b147393f7e18f5b8eca5
WarriusBird <nbgibson@crimson.ua.edu>
2017-11-17 00:56:21 -0500

    Ongoing Test work

--- UnityProject/Assets/Scripts/ActionMaster.cs
+++ UnityProject/Assets/Scripts/ActionMaster.cs
@@ -6,14 +6,31 @@ public class ActionMaster {
 
     public enum Action {Tidy, Reset, EndTurn, EndGame};
 
-    //private int[] bowls = new int[21];
+    private int[] bowls = new int[21];
     private int bowl = 1;
 
     public Action Bowl (int pins)
     {
-        if(pins < 0 || pins > 10)
+        if (pins < 0 || pins > 10)
         {
             throw new UnityException("Invalid pins");
+        }
+
+        bowls[bowl - 1] = pins;
+
+        if(bowl == 21)
+        {
+            return Action.EndGame;
+        }
+
+        if (bowl >= 19 && Bowl21Awarded())
+        {
+            bowl += 1;
+            return Action.Reset;
+        }
+        else if(bowl == 20 && !Bowl21Awarded())
+        {
+            return Action.EndGame;
         }
 
         if(pins == 10)
@@ -31,6 +48,12 @@ public class ActionMaster {
             bowl += 1;
             return Action.EndTurn;
         }
+        //default
         throw new UnityException("Not sure what action to return!");
+    }
+
+    private bool Bowl21Awarded()
+    { //Arrays start at 0
+        return (bowls[19 - 1] + bowls[20 - 1] >= 10);
     }
 }
